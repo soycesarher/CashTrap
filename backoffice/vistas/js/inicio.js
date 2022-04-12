bb8f69149c4ad753ece920bbbde3e53effbc6d5b
soycesarher <soycesarher@gmail.com>
2022-04-12 16:04:04 -0500

    33-organizando-vistas-previas

---
+++ backoffice/vistas/js/inicio.js
@@ -0,0 +1,10 @@
+/*=============================================
+VISTAS PREVIAS DE VIDEOS
+=============================================*/
+
+$(".slideAcademia").jdSlider({
+	wrap: '.slide-inner',
+ 	slideShow: 4,
+ 	slideToScroll: 4,
+ 	isLoop: false
+});
