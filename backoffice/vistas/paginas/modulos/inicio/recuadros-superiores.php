41ce8b527fa597ba5ba065d98d69205ad28f158a
soycesarher <soycesarher@gmail.com>
2022-04-12 15:26:45 -0500

    32-organizando-recuadros-inicio

---
+++ backoffice/vistas/paginas/modulos/inicio/recuadros-superiores.php
@@ -0,0 +1,65 @@
+<div class="row">
+          <div class="col-12 col-sm-6 col-lg-3 ">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>$150</h3>
+
+                <p>Mis comisiones</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-dollar-sign"></i>
+              </div>
+              <a href="ingresos-uninivel" class="small-box-footer">Mas informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+          <div class="col-12 col-sm-6 col-lg-3 ">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53<sup style="font-size: 20px"></sup></h3>
+
+                <p>Mi red</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-sitemap"></i>
+              </div>
+              <a href="uninivel" class="small-box-footer">Mas informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+          <div class="col-12 col-sm-6 col-lg-3 ">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>4</h3>
+
+                <p>Mis tickets</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-comments"></i>
+              </div>
+              <a href="soporte" class="small-box-footer">Mas informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+          <div class="col-12 col-sm-6 col-lg-3 ">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>Activo</h3>
+
+                <p>Renovacion 2022</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user"></i>
+              </div>
+              <a href="perfil" class="small-box-footer">Cancelar suscripcion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
