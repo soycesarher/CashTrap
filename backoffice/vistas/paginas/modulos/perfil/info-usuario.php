89ec21424f3637674c4a00d364c86da8435bd18e
soycesarher <soycesarher@gmail.com>
2022-04-12 19:17:23 -0500

    35-organizando-pagina-perfil

---
+++ backoffice/vistas/paginas/modulos/perfil/info-usuario.php
@@ -0,0 +1,42 @@
+<div class="col-12 col-md-4">
+
+	<div class="card card-info card-outline">
+
+		<div class="card-body box-profile">
+
+			<div class="text-center">
+				
+			 	<img class="profile-user-img img-fluid img-circle" src="vistas/img/usuarios/default/default.png">
+
+			</div>	
+
+			<h3 class="profile-username text-center">
+				
+				Administrador
+
+			</h3>
+
+			<p class="text-muted text-center">
+
+				info@cashtrap.com
+
+			</p>
+
+			<div class="text-center">
+				
+				<button class="btn btn-primary btn-sm">Cambiar foto</button>
+				<button class="btn btn-purple btn-sm">Cambiar contraseña</button>
+
+			</div>
+
+		</div>
+
+		<div class="card-footer">
+
+			<button class="btn btn-default float-right">Eliminar cuenta</button>
+
+		</div>
+
+	</div>	
+	
+</div>
