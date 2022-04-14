4a380b7acd086c583e0345b522dd62086f4aa5f0
soycesarher <soycesarher@gmail.com>
2022-04-14 14:26:39 -0500

    43-registro-usuarios-p1

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/ruta.controlador.php";
+require_once "backoffice/controladores/usuarios.controlador.php";
+require_once "backoffice/modelos/usuarios.modelo.php";
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
