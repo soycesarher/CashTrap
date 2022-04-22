091f92be59118276fa446bd76a7a15fd69a659e8
soycesarher <soycesarher@gmail.com>
2022-04-22 15:34:50 -0500

    134-limpio-de-errores

--- backoffice/pagos/pagos-automaticos-uninivel.php
+++ backoffice/pagos/pagos-automaticos-uninivel.php
@@ -199,13 +199,13 @@ function pagarUsuario($id_usuario, $enlace_afiliado, $paypal, $vencimiento, $fec
 							if($traerPatrocinador["suscripcion"] == 0){
 
 								$patrocinadorRed = "cashtrap-afiliado";
-								//$porcentaje = 1;
+								$porcentaje = 1;
 
 							}else{
 
 								$patrocinadorRed = $traerPatrocinador["enlace_afiliado"];
 
-								/*if($patrocinadorRed == "cashtrap-afiliado"){
+								if($patrocinadorRed == "cashtrap-afiliado"){
 
 									$porcentaje = 1;
 								
@@ -213,7 +213,7 @@ function pagarUsuario($id_usuario, $enlace_afiliado, $paypal, $vencimiento, $fec
 
 									$porcentaje = 0.4;
 
-								}*/
+								}
 
 							}
 
@@ -229,20 +229,20 @@ function pagarUsuario($id_usuario, $enlace_afiliado, $paypal, $vencimiento, $fec
 							BORRAR PATROCINIOS
 							=============================================*/
 
-							//$borrarPartrocinios = ControladorPagos::ctrBorrarPatrociniosRedUninivel($enlace_afiliado);
-							//echo '<pre>Borrar patrocinios Red: '; print_r($borrarPartrocinios); echo '</pre>';
+							$borrarPartrocinios = ControladorPagos::ctrBorrarPatrociniosRedUninivel($enlace_afiliado);
+							echo '<pre>Borrar patrocinios Red: '; print_r($borrarPartrocinios); echo '</pre>';
 
 							/*=============================================
 							ACTUALIZAR TABLA RED
 							=============================================*/
 
-							/*$datosRed = array("usuario_red" => $id_usuario,
+							$datosRed = array("usuario_red" => $id_usuario,
 											  "patrocinador_red" => $patrocinadorRed,
 											  "periodo_comision" => 10*$porcentaje,
 											  "periodo_venta" => 10);
 
 							$actualizarRed = ControladorPagos::ctrActualizarRedUninivel($datosRed);
-							echo '<pre>Actualizar Usuario Red: '; print_r($actualizarRed); echo '</pre>';*/
+							echo '<pre>Actualizar Usuario Red: '; print_r($actualizarRed); echo '</pre>';
 							
 						} else {
 
@@ -250,7 +250,7 @@ function pagarUsuario($id_usuario, $enlace_afiliado, $paypal, $vencimiento, $fec
 							ACTUALIZAR SUSCRIPCIÓN TABLA DE USUARIOS
 							=============================================*/
 
-							/*$datosSuscripcion = array("id_usuario" => $id_usuario,
+							$datosSuscripcion = array("id_usuario" => $id_usuario,
 													  "suscripcion" => 0,
 													  "id_suscripcion" => null,
 													  "vencimiento" => null,
@@ -259,14 +259,14 @@ function pagarUsuario($id_usuario, $enlace_afiliado, $paypal, $vencimiento, $fec
 													  "fecha_contrato" => null);
 
 							$cancelarSuscripcion = ControladorPagos::ctrCancelarSuscripcion($datosSuscripcion);
-							echo '<pre>Cancelar suscripcion: '; print_r($cancelarSuscripcion); echo '</pre>';*/
+							echo '<pre>Cancelar suscripcion: '; print_r($cancelarSuscripcion); echo '</pre>';
 
 							/*=============================================
 							ELIMINAR USUARIO DE LA RED
 							=============================================*/
 
-							/*$eliminarUsuarioRed = ControladorPagos::ctrEliminarUsuarioRed($id_usuario);
-							echo '<pre>Eliminar Usuario Red: '; print_r($eliminarUsuarioRed); echo '</pre>';*/
+							$eliminarUsuarioRed = ControladorPagos::ctrEliminarUsuarioRed($id_usuario);
+							echo '<pre>Eliminar Usuario Red: '; print_r($eliminarUsuarioRed); echo '</pre>';
 
 						}
 
@@ -280,8 +280,51 @@ function pagarUsuario($id_usuario, $enlace_afiliado, $paypal, $vencimiento, $fec
 
 	}
 
-}
+	if($enlace_afiliado == "cashtrap-afiliado"){
 
+		/*=============================================
+		INGRESAR PAGO DE ADMINISTRADOR
+		=============================================*/	
+
+		$tabla = "pagos_uninivel";
+
+		$fechaInicial = strtotime('-1 day', strtotime($vencimiento)); 
+		$fechaInicial = date( 'Y-m-d', $fechaInicial);
+
+		$datos = array("id_pago_paypal" => null,
+					   "usuario_pago" => $id_usuario,
+					   "periodo" => $fechaInicial." a ".$vencimiento,
+					   "periodo_comision" => $periodo_comision,
+					   "periodo_venta" => $periodo_venta);
+		
+		$pagos = ControladorPagos::ctrIngresarPagos($tabla, $datos);
+		echo '<pre>Pago Administrador: '; print_r($pagos); echo '</pre>';
+
+		/*=============================================
+		BORRAR PATROCINIOS
+		=============================================*/
+
+		$borrarPartrocinios = ControladorPagos::ctrBorrarPatrociniosRedUninivel($enlace_afiliado);
+		echo '<pre>Borrar patrocinios Red: '; print_r($borrarPartrocinios); echo '</pre>';
+
+		/*=============================================
+		ACTUALIZAR FECHA DE VENCIMIENTO
+		=============================================*/
+
+		$fechaNuevaVencimiento = strtotime ( '+1 day' , strtotime ($vencimiento));
+		$fechaNuevaVencimiento = date ( 'Y-m-d' , $fechaNuevaVencimiento );	
+
+		$datosSuscripcion = array("id_usuario" => $id_usuario,
+								  "patrocinador" => null,
+								  "ciclo_pago" => null,
+								  "vencimiento" => $fechaNuevaVencimiento);
+
+		$actualizarSuscripcion = ControladorPagos::ctrActualizarSuscripcion($datosSuscripcion);
+		echo '<pre>Actualizar suscripción: '; print_r($actualizarSuscripcion); echo '</pre>';
+
+	}
+
+}
 
 /*=============================================
 CONTROLADORES
@@ -335,6 +378,62 @@ Class ControladorPagos{
 		$tabla = "usuarios";
 
 		$respuesta = ModeloPagos::mdlActualizarSuscripcion($tabla, $datos);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	BORRAR PATROCINIOS
+	=============================================*/
+	
+	static public function ctrBorrarPatrociniosRedUninivel($datos){
+
+		$tabla = "red_uninivel";
+
+		$respuesta = ModeloPagos::mdlBorrarPatrociniosRedUninivel($tabla, $datos);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	ACTUALIZAR RED UNINIVEL
+	=============================================*/
+	
+	static public function ctrActualizarRedUninivel($datos){
+
+		$tabla = "red_uninivel";
+
+		$respuesta = ModeloPagos::mdlActualizarRedUninivel($tabla, $datos);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	Cancelar Suscripción
+	=============================================*/
+
+	static public function ctrCancelarSuscripcion($datos){
+
+		$tabla = "usuarios";
+
+		$respuesta = ModeloPagos::mdlCancelarSuscripcion($tabla, $datos);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	Eliminar Usuario de la red
+	=============================================*/
+
+	static public function ctrEliminarUsuarioRed($datos){
+
+		$tabla = "red_uninivel";
+
+		$respuesta = ModeloPagos::mdlEliminarUsuarioRed($tabla, $datos);
 
 		return $respuesta;
 
@@ -484,6 +583,123 @@ class ModeloPagos{
 		}
 
 		$stmt-> close();
+
+		$stmt = null;
+
+	}
+
+	/*=============================================
+	BORRAR PATROCINIOS
+	=============================================*/
+
+	static public function mdlBorrarPatrociniosRedUninivel($tabla, $datos){
+
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET patrocinador_red = null WHERE patrocinador_red = :patrocinador_red");
+
+		$stmt -> bindParam(":patrocinador_red", $datos, PDO::PARAM_STR);
+
+		if($stmt -> execute()){
+
+			return "ok";
+
+		}else{
+
+			echo "\nPDO::errorInfo():\n";
+    		print_r(Conexion::conectar()->errorInfo());
+
+		}
+
+		$stmt-> close();
+
+		$stmt = null;
+
+	}
+
+	/*=============================================
+	ACTUALIZAR RED UNINIVEL
+	=============================================*/
+
+	static public function mdlActualizarRedUninivel($tabla, $datos){
+
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET patrocinador_red =:patrocinador_red, periodo_comision =:periodo_comision, periodo_venta = :periodo_venta WHERE usuario_red = :usuario_red");
+		
+		$stmt -> bindParam(":patrocinador_red", $datos["patrocinador_red"], PDO::PARAM_STR);
+		$stmt -> bindParam(":periodo_comision", $datos["periodo_comision"], PDO::PARAM_STR);
+		$stmt -> bindParam(":periodo_venta", $datos["periodo_venta"], PDO::PARAM_STR);
+		$stmt -> bindParam(":usuario_red", $datos["usuario_red"], PDO::PARAM_STR);
+
+		if($stmt -> execute()){
+
+			return "ok";
+
+		}else{
+
+			echo "\nPDO::errorInfo():\n";
+    		print_r(Conexion::conectar()->errorInfo());
+
+		}
+
+		$stmt-> close();
+
+		$stmt = null;
+
+	}
+
+	/*=============================================
+	Cancelar Suscripción
+	=============================================*/
+
+	static public function mdlCancelarSuscripcion($tabla, $datos){
+
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET  suscripcion = :suscripcion, id_suscripcion = :id_suscripcion, vencimiento = :vencimiento, ciclo_pago = :ciclo_pago, firma = :firma, fecha_contrato = :fecha_contrato WHERE id_usuario = :id_usuario");
+
+		$stmt -> bindParam(":suscripcion", $datos["suscripcion"], PDO::PARAM_STR);
+		$stmt -> bindParam(":id_suscripcion", $datos["id_suscripcion"], PDO::PARAM_STR);
+		$stmt -> bindParam(":vencimiento", $datos["vencimiento"], PDO::PARAM_STR);
+		$stmt -> bindParam(":ciclo_pago", $datos["ciclo_pago"], PDO::PARAM_STR);
+		$stmt -> bindParam(":firma", $datos["firma"], PDO::PARAM_STR);
+		$stmt -> bindParam(":fecha_contrato", $datos["fecha_contrato"], PDO::PARAM_STR);
+		$stmt -> bindParam(":id_usuario", $datos["id_usuario"], PDO::PARAM_INT);
+
+		if($stmt -> execute()){
+
+			return "ok";
+
+		}else{
+
+			echo "\nPDO::errorInfo():\n";
+    		print_r(Conexion::conectar()->errorInfo());
+
+		}
+
+		$stmt-> close();
+
+		$stmt = null;
+
+	}
+
+	/*=============================================
+	Eliminar Usuario de la Red
+	=============================================*/
+
+	static public function mdlEliminarUsuarioRed($tabla, $datos){
+
+		$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE usuario_red = :usuario_red");
+
+		$stmt -> bindParam(":usuario_red", $datos, PDO::PARAM_INT);
+
+		if($stmt -> execute()){
+
+			return "ok";
+		
+		}else{
+
+			echo "\nPDO::errorInfo():\n";
+    		print_r(Conexion::conectar()->errorInfo());
+
+		}
+
+		$stmt -> close();
 
 		$stmt = null;
 
--- cashtrap.sql
+++ cashtrap.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-04-2022 a las 21:33:33
+-- Tiempo de generación: 22-04-2022 a las 22:33:21
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
@@ -67,7 +67,7 @@ CREATE TABLE `pagos_uninivel` (
 --
 
 INSERT INTO `pagos_uninivel` (`id_pago`, `id_pago_paypal`, `usuario_pago`, `periodo`, `periodo_comision`, `periodo_venta`, `fecha_pago`) VALUES
-(1, '83TEPJXBQJXZE', 27, '2022-03-21 a 2022-04-21', 4, 10, '2022-04-22 19:29:40');
+(4, '83TEPJXBQJXZE', 27, '2022-03-21 a 2022-04-21', 4, 10, '2022-04-22 20:32:23');
 
 -- --------------------------------------------------------
 
@@ -78,7 +78,7 @@ INSERT INTO `pagos_uninivel` (`id_pago`, `id_pago_paypal`, `usuario_pago`, `peri
 CREATE TABLE `red_uninivel` (
   `id_uninivel` int(11) NOT NULL,
   `usuario_red` int(11) NOT NULL,
-  `patrocinador_red` text NOT NULL,
+  `patrocinador_red` text DEFAULT NULL,
   `periodo_comision` float NOT NULL,
   `periodo_venta` float NOT NULL,
   `fecha_uninivel` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -90,7 +90,7 @@ CREATE TABLE `red_uninivel` (
 
 INSERT INTO `red_uninivel` (`id_uninivel`, `usuario_red`, `patrocinador_red`, `periodo_comision`, `periodo_venta`, `fecha_uninivel`) VALUES
 (4, 27, 'cashtrap-afiliado', 10, 10, '2022-04-22 18:47:57'),
-(5, 28, 'cesar-hernandez-27', 4, 10, '2022-04-22 18:50:44');
+(5, 28, NULL, 4, 10, '2022-04-22 20:32:24');
 
 -- --------------------------------------------------------
 
@@ -128,7 +128,7 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id_usuario`, `perfil`, `nombre`, `email`, `password`, `suscripcion`, `id_suscripcion`, `ciclo_pago`, `vencimiento`, `verificacion`, `email_encriptado`, `foto`, `enlace_afiliado`, `patrocinador`, `paypal`, `pais`, `codigo_pais`, `telefono_movil`, `firma`, `fecha_contrato`, `fecha`) VALUES
 (1, 'admin', 'cashtrap', 'admin@cashtrap.com', 'admin', 1, NULL, NULL, NULL, 1, NULL, NULL, 'cashtrap-afiliado', NULL, 'soycesarher@gmail.com', NULL, NULL, NULL, NULL, NULL, '2022-04-14 18:55:23'),
-(27, 'usuario', 'Cesar Hernandez', 'imcesar@gmail.com', '123456', 1, 'I-35W0EREPJ9NC', 1, '2022-04-21', 1, 'd46dc768540910fb47a39d5f32cbef5b', NULL, 'cesar-hernandez-27', 'cashtrap-afiliado', 'sb-04sxl15100009@personal.example.com', 'United States', 'US', '+1 (212) 324-4152', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"145\" height=\"43\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 14 2 c 2.28 0.46 127.7 25.72 130 26 c 0.9 0.11 -32.15 -11.34 -49 -16 c -15.07 -4.17 -29.96 -7.53 -45 -10 c -7.19 -1.18 -18.42 -3.91 -22 -1 c -4.68 3.8 -9.24 20.92 -10 27 c -0.21 1.65 3.91 3.88 6 5 c 1.95 1.05 4.7 1.65 7 2 c 1.89 0.29 4.32 0.63 6 0 c 5.62 -2.11 12.88 -5.74 18 -9 c 1.63 -1.04 2.53 -3.53 4 -5 c 1.47 -1.47 3.69 -2.46 5 -4 c 2.21 -2.61 5.86 -7.58 6 -9 c 0.07 -0.67 -3.43 -1.14 -5 -1 c -5.63 0.49 -12.21 1.47 -18 3 c -5.43 1.44 -10.65 3.81 -16 6 c -2.1 0.86 -4.22 1.73 -6 3 c -5.1 3.65 -10.47 7.66 -15 12 l -9 11\"/></svg>', '2022-04-22', '2022-04-22 18:45:09'),
+(27, 'usuario', 'Cesar Hernandez', 'imcesar@gmail.com', '123456', 1, 'I-35W0EREPJ9NC', 1, '2022-05-22', 1, 'd46dc768540910fb47a39d5f32cbef5b', NULL, 'cesar-hernandez-27', 'cashtrap-afiliado', 'sb-04sxl15100009@personal.example.com', 'United States', 'US', '+1 (212) 324-4152', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"145\" height=\"43\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 14 2 c 2.28 0.46 127.7 25.72 130 26 c 0.9 0.11 -32.15 -11.34 -49 -16 c -15.07 -4.17 -29.96 -7.53 -45 -10 c -7.19 -1.18 -18.42 -3.91 -22 -1 c -4.68 3.8 -9.24 20.92 -10 27 c -0.21 1.65 3.91 3.88 6 5 c 1.95 1.05 4.7 1.65 7 2 c 1.89 0.29 4.32 0.63 6 0 c 5.62 -2.11 12.88 -5.74 18 -9 c 1.63 -1.04 2.53 -3.53 4 -5 c 1.47 -1.47 3.69 -2.46 5 -4 c 2.21 -2.61 5.86 -7.58 6 -9 c 0.07 -0.67 -3.43 -1.14 -5 -1 c -5.63 0.49 -12.21 1.47 -18 3 c -5.43 1.44 -10.65 3.81 -16 6 c -2.1 0.86 -4.22 1.73 -6 3 c -5.1 3.65 -10.47 7.66 -15 12 l -9 11\"/></svg>', '2022-04-22', '2022-04-22 18:45:09'),
 (28, 'usuario', 'Andrea Lopez', 'imandrea@gmail.com', '123456', 1, 'I-L0N8N9H4KC5D', 1, '2022-05-22', 1, '31f6650277d792c6069be1a6ff41f349', NULL, 'andrea-lopez-28', 'cesar-hernandez-27', 'sb-04sxl15100009@personal.example.com', 'United States', 'US', '+1 (272) 456-1221', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"132\" height=\"38\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 131 21 c -0.89 -0.16 -33.93 -7.76 -51 -9 c -24.07 -1.75 -57.99 -3.42 -73 0 c -3.9 0.89 -7.89 15.58 -6 18 c 2.45 3.13 15.85 3.86 24 5 c 8.67 1.21 17.24 1.81 26 2 c 6.72 0.15 13.8 0.21 20 -1 c 5.29 -1.03 10.78 -3.71 16 -6 c 3.14 -1.38 6.13 -3.16 9 -5 c 1.78 -1.15 3.89 -2.38 5 -4 c 3.56 -5.22 6.66 -13.22 10 -18 c 0.73 -1.04 2.9 -2 4 -2 c 1.1 0 3.06 1.06 4 2 l 6 8\"/></svg>', '2022-04-22', '2022-04-22 18:49:13');
 
 -- --------------------------------------------------------
@@ -221,7 +221,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `pagos_uninivel`
 --
 ALTER TABLE `pagos_uninivel`
-  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `red_uninivel`
